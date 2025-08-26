19d1ba4bfbda551cdf21914330847980cef0514c
Emiliano Romero <diego.romero@keyinstitute.edu.sv>
2025-08-25 21:51:57 -0600

    Update Reto 1. Biblioteca ER.cpp

--- Reto 1. Biblioteca ER.cpp
+++ Reto 1. Biblioteca ER.cpp
@@ -3,18 +3,20 @@
 
 # include <iostream>
 # include <string>
+# include <vector>
 
 // Crear la clase Libro ( L I S T O ! )
 
 class Libro {
     public:
-        std::string titulo;
-        std::string autor;
-        int anopublicacion;
+        const std::string titulo;
+        const std::string autor;
+        const int anopublicacion;
         bool estadisponible;
 
     // Constructor
-    Libro() : anopublicacion(0), estadisponible(true) {}
+     Libro(const std::string& titulo, const std::string& autor, int anopublicacion)
+        : titulo(titulo), autor(autor), anopublicacion(anopublicacion), estadisponible(true) {}
 
     void mostrarDetallesCompletos() {
         std::cout << "Titulo: " << titulo << std::endl;
@@ -27,3 +29,39 @@ class Libro {
     }
         }
 };
+
+// Crear la clase Biblioteca
+
+class Biblioteca {
+
+    public:
+        std::vector<Libro> libros;
+        Biblioteca(std::vector<Libro>& iniciales) : libros(iniciales) {}
+};
+
+
+// M A I N
+int main() {
+    std::vector<Libro> misLibros = {
+        Libro("Cien años de soledad", "Gabriel García Márquez", 1967),
+        Libro("El Principito", "Antoine de Saint-Exupéry", 1943),
+        Libro("Don Quijote de la Mancha", "Miguel de Cervantes", 1605),
+        Libro("1984", "George Orwell", 1949),
+        Libro("Rayuela", "Julio Cortázar", 1963),
+        Libro("Orgullo y prejuicio", "Jane Austen", 1813),
+        Libro("Crimen y castigo", "Fiódor Dostoyevski", 1866),
+        Libro("La Odisea", "Homero", -800),
+        Libro("Fahrenheit 451", "Ray Bradbury", 1953),
+        Libro("El Hobbit", "J.R.R. Tolkien", 1937)
+    };
+
+    Biblioteca miBiblioteca(misLibros);
+
+    // Mostrar detalles de cada libro
+    for (const auto& libro : miBiblioteca.libros) {
+        libro.mostrarDetallesCompletos();
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
