47b19f83fd4ae1cf695e0b4458b9d7ab71c84cbd
Emiliano Romero <diego.romero@keyinstitute.edu.sv>
2025-08-25 22:56:02 -0600

    Update Reto 1. Biblioteca ER.cpp

--- Reto 1. Biblioteca ER.cpp
+++ Reto 1. Biblioteca ER.cpp
@@ -20,7 +20,7 @@ class Libro {
     void mostrarDetallesCompletos() {
         std::cout << "Titulo: " << titulo << std::endl;
         std::cout << "Autor: " << autor << std::endl;
-        std::cout << "Año de publicación: " << anopublicacion << std::endl;
+        std::cout << "Ano de publicación: " << anopublicacion << std::endl;
         if (estadisponible) {
             std::cout << "Disponiblidad: Sí hay!" << std::endl;
     }   else {
@@ -40,10 +40,7 @@ class Biblioteca {
     // M É T O D O S    D E    L A    L I B R E R í A
 
     void agregar_libro(const Libro& nuevoLibro) {
-
-        libros.push_back(nuevoLibro);
-        std::cout << "El libro ha sido agregado a la librería exitosamente :)" << std::endl;
-
+        
         bool existe = false;
 
         for (const auto& libro : libros) {
@@ -109,7 +106,7 @@ class Biblioteca {
 int main() {
 
     std::vector<Libro> misLibros = {
-        Libro("Cien años de soledad", "Gabriel García Márquez", 1967),
+        Libro("Cien anos de soledad", "Gabriel García Márquez", 1967),
         Libro("El Principito", "Antoine de Saint-Exupéry", 1943),
         Libro("Don Quijote de la Mancha", "Miguel de Cervantes", 1605),
         Libro("1984", "George Orwell", 1949),
@@ -146,7 +143,7 @@ int main() {
             std::cout << "Autor: ";
             std::getline(std::cin >> std::ws, autor);
 
-            std::cout << "Año de publicación: ";
+            std::cout << "Ano de publicación: ";
             std::cin >> anopublicacion;
 
             Libro nuevoLibro(titulo, autor, anopublicacion);
